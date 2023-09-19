5251701d1ba1e1c3eb6b927a394a2ecadb8b24c3
GrGrino <123995503+GrGrino@users.noreply.github.com>
2023-09-19 17:45:39 +0200

    Add files via upload

---
+++ Java/SAV.java
@@ -0,0 +1,30 @@
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+
+public class SAV extends JFrame{
+
+    
+    public SAV(){
+        setTitle("SAV");
+		setSize(700,500);
+		setLocationRelativeTo(null);
+
+		JPanel pan=new JPanel();
+		pan.setLayout(null);
+        
+
+    }
+
+    public static void main(String[] args) {
+        new SAV();
+    }
+}
